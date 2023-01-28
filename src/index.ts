42eda932593baa28d232de5c7f693626f939600e
daniel shokri <danielshokri@gmail.com>
2023-01-28 20:58:16 +0200

    adding prisma migrate

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import express from 'express';
 import morgan from 'morgan';
+import { PrismaClient } from '@prisma/client';
 
+const prisma = new PrismaClient();
 const app = express();
 app.use(morgan('dev'));
 
-app.get('/', (req, res) => {
-  res.json({ hello: 'world!!!' });
+app.get('/', async (req, res) => {
+  const posts = await prisma.post.findMany();
+  res.json(posts);
+  // res.json({ hello: 'world!!!' });
 });
 
 const port = Number(process.env.PORT ?? 8080);
